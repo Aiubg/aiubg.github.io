b58f6310be9bf4f3f59d8fad3de0a6ab55d76427
gxqmars <3555439401@qq.com>
2025-04-20 11:40:39 +0800

    8

--- docs/.vitepress/configs/zhThemeConfig.js
+++ docs/.vitepress/configs/zhThemeConfig.js
@@ -1,50 +1,54 @@
-const navOptions = [{
-  text: "Vue.js",
-  link: "/Vue.js/Vue入门/01-从零开始理解Vue3",
-},
-{
-  text: '考研数学',
-  items: [
-    { text: '高等数学', link: '/考研数学/高等数学/函数' },
-    { text: '概率论', link: '/考研数学/概率论' },
-    { text: '线性代数', link: '/考研数学/线性代数' }
-  ]
-},{
-  text: "考研规划",
-  link: "/考研规划/考研规划",
-  target: "_blank",
-},
-{
-  text: "考研倒计时",
-  link: "/考研规划/考研倒计时",
-  target: "_blank",
-},
-{
-  text: "词汇积累",
-  link: "/词汇积累/词汇积累",
-}
+const navOptions = [
+  {
+    text: "Vue.js",
+    link: "/Vue.js/Vue入门/01-从零开始理解Vue3",
+  },
+  {
+    text: "考研数学",
+    items: [
+      { text: "高等数学", link: "/考研数学/高等数学/函数" },
+      { text: "概率论", link: "/考研数学/概率论" },
+      { text: "线性代数", link: "/考研数学/线性代数" },
+    ],
+  },
+  {
+    text: "考研规划",
+    link: "/考研规划/考研规划",
+    target: "_blank",
+  },
+
+  {
+    text: "词汇积累",
+    link: "/词汇积累/词汇积累",
+  },
 ]
 const sidebarOptions = {
   "/Vue.js/": [
     {
       text: "Vue 入门",
       items: [
-        { text: "从零开始理解Vue3", link: "/Vue.js/Vue入门/01-从零开始理解Vue3" },
-        { text: "组件系统与生命周期", link: "/Vue.js/Vue入门/02-组件系统与生命周期" },
-        { text: "组合式API与响应式进阶", link: "/Vue.js/Vue入门/03-组合式API与响应式进阶" },
-        { text: "路由与状态管理", link: "/Vue.js/Vue入门/04-路由与状态管理" }
+        {
+          text: "从零开始理解Vue3",
+          link: "/Vue.js/Vue入门/01-从零开始理解Vue3",
+        },
+        {
+          text: "组件系统与生命周期",
+          link: "/Vue.js/Vue入门/02-组件系统与生命周期",
+        },
+        {
+          text: "组合式API与响应式进阶",
+          link: "/Vue.js/Vue入门/03-组合式API与响应式进阶",
+        },
+        { text: "路由与状态管理", link: "/Vue.js/Vue入门/04-路由与状态管理" },
       ],
     },
   ],
-  "/高等数学/":[
+  "/高等数学/": [
     {
       text: "高等数学",
-      items: [
-        { text: "数学分析", link: "/考研数学/高等数学/函数" },
-      ],
+      items: [{ text: "数学分析", link: "/考研数学/高等数学/函数" }],
     },
-  ]
-
+  ],
 }
 const socialLinkOptions = [{ icon: "github", link: "https://github.com/Aiubg" }]
 const themeConfig = {
